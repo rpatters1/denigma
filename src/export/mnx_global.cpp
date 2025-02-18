9773aebc5ea6d75d8993c111d3148bdfd7be2ba2
Robert Patterson <robert@robertgpatterson.com>
2025-02-18 14:21:08 -0600

    refactor for breaking changes

--- src/export/mnx_global.cpp
+++ src/export/mnx_global.cpp
@@ -69,7 +69,7 @@ static void createEnding(mnx::global::Measure& mnxMeasure, const std::shared_ptr
             auto mnxEnding = mnxMeasure.create_ending(musxEnding->calcEndingLength());
             mnxEnding.set_open(musxEnding->calcIsOpen());
             if (auto musxNumbers = musxMeasure->getDocument()->getOthers()->get<others::RepeatPassList>(SCORE_PARTID, musxMeasure->getCmper())) {
-                for (int value : musxNumbers->m_endingNumbers) {
+                for (int value : musxNumbers->endingNumbers) {
                     if (!mnxEnding.numbers().has_value()) {
                         mnxEnding.create_numbers();
                     }
