b0aa765e3d664ecd0b33a142ea020142a7b28bd8
Robert Patterson <robert@robertgpatterson.com>
2026-05-18 11:14:55 -0500

    add nonArpeggios from expression assignments

--- src/export/mnx_parts.cpp
+++ src/export/mnx_parts.cpp
@@ -259,26 +259,33 @@ static void processExpressions(const MnxMusxMappingPtr& context, const MusxInsta
             if (!asgn->calcIsAssignedInRequestedPart()) {
                 continue;
             }
-            if (asgn->staffAssign == staffCmper && asgn->textExprId && !asgn->hidden) {
-                if (auto expr = asgn->getTextExpression()) {
-                    if (auto text = expr->getTextBlock()) {
-                        if (auto rawTextCtx = text->getRawTextCtx(SCORE_PARTID)) {
-                            auto fontInfo = rawTextCtx.parseFirstFontInfo();
-                            std::string exprText = rawTextCtx.getText(true, musx::util::EnigmaString::AccidentalStyle::Unicode);
-                            const auto attachment = calcAttachmentContext(asgn);
-                            /// @todo Add calculation for above or below, rather than just Float, for marking placement
-                            if (isDynamicExpression(expr)) { /// @todo: be smarter about identifying dynamics
-                                appendDynamic(asgn, fontInfo, exprText);
-                            } else if (auto fermata = calcFermata(fontInfo, exprText)) {
-                                attachFermata(asgn, expr, attachment, fermata.value());
-                            } else if (auto breathMark = calcBreathMark(fontInfo, exprText)) {
-                                attachBreathMark(attachment, breathMark.value());
+            if (asgn->staffAssign == staffCmper && !asgn->hidden) {
+                if (asgn->textExprId) {
+                    if (auto expr = asgn->getTextExpression()) {
+                        if (auto text = expr->getTextBlock()) {
+                            if (auto rawTextCtx = text->getRawTextCtx(SCORE_PARTID)) {
+                                auto fontInfo = rawTextCtx.parseFirstFontInfo();
+                                std::string exprText = rawTextCtx.getText(true, musx::util::EnigmaString::AccidentalStyle::Unicode);
+                                const auto attachment = calcAttachmentContext(asgn);
+                                /// @todo Add calculation for above or below, rather than just Float, for marking placement
+                                if (isDynamicExpression(expr)) { /// @todo: be smarter about identifying dynamics
+                                    appendDynamic(asgn, fontInfo, exprText);
+                                } else if (auto fermata = calcFermata(fontInfo, exprText)) {
+                                    attachFermata(asgn, expr, attachment, fermata.value());
+                                } else if (auto breathMark = calcBreathMark(fontInfo, exprText)) {
+                                    attachBreathMark(attachment, breathMark.value());
+                                }
+                            } else {
+                                context->logMessage(LogMsg() << "Text block " << text->getCmper() << " has non-existent raw text block " << text->textId, LogSeverity::Warning);
                             }
                         } else {
-                            context->logMessage(LogMsg() << "Text block " << text->getCmper() << " has non-existent raw text block " << text->textId, LogSeverity::Warning);
+                            context->logMessage(LogMsg() << "Text expression " << expr->getCmper() << " has non-existent text block " << expr->textIdKey, LogSeverity::Warning);
                         }
-                    } else {
-                        context->logMessage(LogMsg() << "Text expression " << expr->getCmper() << " has non-existent text block " << expr->textIdKey, LogSeverity::Warning);
+                    }
+                }
+                if (asgn->shapeExprId) {                    
+                    if (const auto candidate = musx::util::calcNonArpeggioSpanForAssignment(asgn)) {
+                        appendArpeggioCandidate(context, mnxMeasure, candidate.value());
                     }
                 }
             }
@@ -304,11 +311,8 @@ static void processSmartShapes(
             }
             if (auto shape = context->document->getOthers()->get<others::SmartShape>(SCORE_PARTID, assign->shapeNum)) {
                 if (shape->startTermSeg->endPoint->staffId == staffCmper) {
-                    const auto sourceEntry = shape->startTermSeg->endPoint->calcAssociatedEntry(/*findExact*/true);
-                    if (sourceEntry) {
-                        if (const auto nonArpeggio = musx::util::calcNonArpeggioSpanForSmartShape(sourceEntry, shape)) {
-                            appendArpeggioCandidate(context, mnxMeasure, nonArpeggio.value());
-                        }
+                    if (const auto nonArpeggio = musx::util::calcNonArpeggioSpanForSmartShape(shape)) {
+                        appendArpeggioCandidate(context, mnxMeasure, nonArpeggio.value());
                     }
                 }
             }
@@ -377,6 +381,9 @@ static void createMeasures(const MnxMusxMappingPtr& context, mnx::Part& part)
     }
     for (const auto& musxMeasure : musxMeasures) {
         auto mnxMeasure = mnxMeasures.at(context->currMeas++);
+        if (const auto partId = part.id()) {
+            mnxMeasure.set_id(partId.value() + "." + calcGlobalMeasureId(musxMeasure->getCmper()));
+        }
         for (size_t x = 0; x < context->currPartStaves.size(); x++) {
             context->currStaff = context->currPartStaves[x];
             std::optional<int> staffNumber = (context->currPartStaves.size() > 1) ? std::optional<int>(int(x) + 1) : std::nullopt;
