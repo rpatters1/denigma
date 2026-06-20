102aba15d0858e935fa8e9bf93f3c6b9ccd30ef6
Robert Patterson <robert@robertgpatterson.com>
2026-06-20 09:53:30 -0500

    Add support for hairpin dynamics

--- src/formats/mnx/mnx_parts.cpp
+++ src/formats/mnx/mnx_parts.cpp
@@ -28,6 +28,7 @@
 
 #include "mnx.h"
 #include "mnx_expressions.h"
+#include "mnx_smartshapes.h"
 
 #include "denigma/classify/clefs.h"
 #include "denigma/classify/dynamics.h"
@@ -210,71 +211,6 @@ static void createClefs(
     }
 }
 
-static void processSmartShapes(
-    const MnxMusxMappingPtr& context,
-    mnx::part::Measure mnxMeasure,
-    const MusxInstance<others::Measure>& musxMeasure)
-{
-    const StaffCmper staffCmper = context->current.staff;
-    if (musxMeasure->hasSmartShape) {
-        const auto assigns = context->document->getOthers()->getArray<others::SmartShapeMeasureAssign>(musxMeasure->getRequestedPartId(), musxMeasure->getCmper());
-        for (const auto& assign : assigns) {
-            MUSX_ASSERT_IF(!assign) {
-                context->logMessage(LogMsg() << "skipping empty smart shape assignment for measure " << musxMeasure->getCmper(), MessageSeverity::Warning);
-                continue;
-            }
-            if (assign->centerShapeNum != 0) {
-                // ignore assignments in the middle of the shape
-                continue;
-            }
-            if (auto shape = context->document->getOthers()->get<others::SmartShape>(SCORE_PARTID, assign->shapeNum)) {
-                if (shape->startTermSeg->endPoint->staffId == staffCmper) {
-                    if (const auto nonArpeggio = musx::util::calcNonArpeggioSpanForSmartShape(shape)) {
-                        appendArpeggioCandidate(context, mnxMeasure, nonArpeggio.value());
-                    }
-                }
-            }
-        }
-    }
-}
-
-static void createOttavas(const MnxMusxMappingPtr& context, const MusxInstance<others::Measure>& musxMeasure,
-    mnx::part::Measure& mnxMeasure, std::optional<int> mnxStaffNumber)
-{
-    const StaffCmper staffCmper = context->current.staff;
-    context->current.ottavasApplicableInMeasure.clear();
-    if (musxMeasure->hasSmartShape) {
-        auto shapeAssigns = context->document->getOthers()->getArray<others::SmartShapeMeasureAssign>(musxMeasure->getRequestedPartId(), musxMeasure->getCmper());
-        for (const auto& asgn : shapeAssigns) {
-            if (auto shape = context->document->getOthers()->get<others::SmartShape>(asgn->getRequestedPartId(), asgn->shapeNum)) {
-                if (!shape->hidden && (shape->startTermSeg->endPoint->staffId == staffCmper || shape->endTermSeg->endPoint->staffId == staffCmper)) {
-                    switch (shape->shapeType) {
-                        default: continue;
-                        case others::SmartShape::ShapeType::OctaveDown:
-                        case others::SmartShape::ShapeType::OctaveUp:
-                        case others::SmartShape::ShapeType::TwoOctaveDown:
-                        case others::SmartShape::ShapeType::TwoOctaveUp:
-                            break;
-                    }
-                    context->current.ottavasApplicableInMeasure.emplace(shape->getCmper(), shape);
-                    if (!asgn->centerShapeNum && shape->startTermSeg->endPoint->measId == musxMeasure->getCmper()) {
-                        auto mnxOttava = mnxMeasure.ensure_ottavas().append(
-                            enumConvert<mnx::OttavaAmount>(shape->shapeType),
-                            mnxFractionFromSmartShapeEndPoint(shape->startTermSeg->endPoint),
-                            calcGlobalMeasureId(shape->endTermSeg->endPoint->measId),
-                            mnxFractionFromSmartShapeEndPoint(shape->endTermSeg->endPoint));
-                        mnxOttava.end().position().set_graceIndex(0);   // guarantees inclusion of any grace notes at the end of the ottava
-                        if (mnxStaffNumber) {
-                            mnxOttava.set_staff(mnxStaffNumber.value());
-                        }
-                        /// @todo: orient (if applicable)
-                    }
-                }
-            }
-        }
-    }
-}
-
 static MusxInstance<others::StaffComposite> findCompositeForIdentity(
     const InstrumentInfo& instInfo,
     const InstrumentInfo::InstrumentIdentity& identity)
@@ -346,7 +282,7 @@ static void createMeasures(const MnxMusxMappingPtr& context, mnx::Part& part)
             createSequences(context, mnxMeasure, staffNumber, musxMeasure);
             // the following must come after sequences, in case we need to attach to events (e.g., fermatas);
             processExpressions(context, musxMeasure, mnxMeasure, staffNumber);
-            processSmartShapes(context, mnxMeasure, musxMeasure);
+            processSmartShapes(context, musxMeasure, mnxMeasure, staffNumber);
         }
     }
     context->clearCounts();
